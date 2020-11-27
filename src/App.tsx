a69dd248ddf8b3f8f93a128c9a04fea6b25952da
郑喜荣 <zhengxirong@yanpin.cn>
2020-11-27 20:44:15 +0800

    add menus config.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Home from "./pages/home/Home";
+import HelloWorld from "./pages/hello/HelloWorld";
+
+const styles = {
+    fontFamily: "sans-serif",
+    paddingLeft: "250px",
+};
+
+const routeMap = new Map([
+    ["home", Home],
+    ["hello-world", HelloWorld]
+])
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let clickMenu = (key: string) => {
+        window.history.pushState(null, "", `/#/${key}`)
+        setCurrentPage(key)
+    }
+
+    const [currentPage, setCurrentPage] = useState("home")
+
+    // const currentPage: string = document.location.hash.replace(/#\/?/, "");
+    let CurrentPage = routeMap.get(currentPage) || Home
+
+    return (
+        <div style={styles}>
+            <ul className="menu-list">
+                {Array.from(routeMap.keys()).map(key => (
+                    <li
+                        key={key}
+                        className={key === currentPage ? "is-active" : ""}
+                        style={{listStyle: "none"}}
+                    >
+                        <span className="link" onClick={() => clickMenu(key)}>
+                            {key}
+                        </span>
+                    </li>
+                ))}
+            </ul>
+            <div style={{padding: "30px 0"}}>
+                <CurrentPage/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
